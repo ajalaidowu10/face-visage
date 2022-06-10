4692208e4d0d6635b23352c5f54749269c95ad2e
Ajala Idowu <ajalaidowu10@gmail.com>
2022-06-10 10:18:18 +0100

    Fix frontend issue

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
 		return(
 
 				<nav style={{ display: 'flex', justifyContent: 'flex-end'}}>
-					<button type="button" className="btn btn-link text-danger p-3 " onClick={() => onRouteChange('home')}>Sign in</button>
+					<button type="button" className="btn btn-link text-danger p-3 " onClick={() => onRouteChange('signin')}>Sign in</button>
 					<button type="button" className="btn btn-link text-danger p-3 " onClick={() => onRouteChange('signup')}>Sign up</button>
 				</nav>
 			);
